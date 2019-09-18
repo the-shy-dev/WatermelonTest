6aaabb206ec409e969b6d7a10c6c977dce926f33
acsrikar279 <acsrikar@gmail.com>
2019-09-19 00:23:12 +0530

    shifted

--- src/model/Website.js
+++ src/model/Website.js
@@ -1,8 +1,14 @@
 import { Model } from '@nozbe/watermelondb'
+import { field } from '@nozbe/watermelondb/decorators'
 
 export default class Website extends Model {
     static table = 'websites'
     static associations = {
         users: {type: 'has_many', foreignKey: 'website_id'}
     }
+
+    @field('title') title
+    @field('url') url
+
+    @children('users') users
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
+const webpack = require("webpack");
+
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.worker\.js$/,
-                use: { loader: 'worker-loader' }
-            }
-        ]
-    },
-    output: {
-        globalObject: 'this',
-    }
-}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    globalObject: "this"
+  }
+};
